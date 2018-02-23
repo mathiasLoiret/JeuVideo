4417c87c587655891789a03755703f091cb46227
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-02-23 09:03:34 +0100

    add kevin folder

---
+++ Assets/kevinPautonnier/manageParticuleSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manageParticuleSystem : MonoBehaviour {
+
+	public ParticleSystem ps;
+	public ParticleSystem.EmissionModule emission;
+
+	public float acc=0;
+
+	// Use this for initialization
+	void Start () {
+		emission = ps.emission;
+        emission.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acc += Time.deltaTime;
+		if(acc > 0.4f){
+			emission.enabled = false;
+		}
+		if(Input.GetKeyDown("space")){
+			emission.enabled = true;
+			acc = 0;
+		}
+	}
+}
