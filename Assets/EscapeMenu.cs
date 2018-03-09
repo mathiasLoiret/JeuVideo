9ff2c1b14c64cf0e67d96624814b0e7b9e4e039a
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-03-09 12:51:23 +0100

    esc menu

---
+++ Assets/EscapeMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EscapeMenu : MonoBehaviour {
+
+
+	public Button restart;
+	public Button menuPincip;
+	public bool escapeMenuOpen; 
+	public Image panel;
+	private Scene scene; 
+	
+	
+	// Update is called once per frame
+	void Update () {
+		scene = SceneManager.GetActiveScene();
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if(escapeMenuOpen == false){
+				escapeMenuOpen = true;
+				restart.gameObject.SetActive(true);
+				menuPincip.gameObject.SetActive(true);
+				panel.gameObject.SetActive(true);
+			}else{
+				escapeMenuOpen = false;
+				restart.gameObject.SetActive(false);
+				menuPincip.gameObject.SetActive(false);		
+				panel.gameObject.SetActive(false);	
+			}
+		}else{
+
+		}
+
+	}
+
+	public void GoToMenuPrincip(){
+		SceneManager.LoadScene("MenuPrincp");
+	} 	
+
+	public void RestartLevel(){
+		SceneManager.LoadScene(scene.name);
+	}
+}
