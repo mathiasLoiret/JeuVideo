fc7bbb0c6febf184f87ac1c9c2f16eea367f640f
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-03-09 15:22:10 +0100

    adding music

---
+++ Assets/launch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class launch : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) ){
+			SceneManager.LoadScene("lvl1");
+			
+		}
+	}
+}
