8ecd03eeeb4dbf94fbeb6a32a6070a8c99b0fb64
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-03-15 12:05:09 +0100

    Adding basic timer

---
+++ Assets/scripts/TimeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+	private float time;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		text.text = "" + Mathf.Round(time);	
+	}
+}
