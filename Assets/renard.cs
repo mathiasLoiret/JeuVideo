018c989ba5ffbe917eb712d39f6b4fa57defb2e8
Matthieu Fournier <mat.fournier85@gmail.com>
2018-03-15 10:38:59 +0100

    create prefab renard

--- Assets/renard.cs
+++ Assets/renard.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class renard : MonoBehaviour {
 
-    //public 
+    public Sprite[] renardAnimation;
+
+    private SpriteRenderer sr;
+    private int i;
+
+    private float timer;
+    private float fps;
 
 	// Use this for initialization
 	void Start () {
-		
+        sr = GetComponent<SpriteRenderer>();
+        i = 0;
+        fps = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            i = (i + 1) % renardAnimation.Length;
+            sr.sprite = renardAnimation[i];
+            timer += 1 / fps;
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
