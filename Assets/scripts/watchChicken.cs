a5fba09634b79ccf4d75bfbea8b0e06486c8a04e
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-03-15 12:41:20 +0100

    bee watch fox

---
+++ Assets/scripts/watchChicken.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watchChicken : MonoBehaviour {
+
+	private SpriteRenderer sr;
+	public Transform chickenTransform;
+	private Transform beeTransform;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		beeTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(beeTransform.position.x > chickenTransform.position.x){
+			sr.flipX = false;
+		}
+		else{
+			sr.flipX = true;
+		}
+	}
+}
