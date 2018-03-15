9af02351a62f0adb730c89eddc52ed617e41ebbf
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-03-15 10:32:54 +0100

    change camera

---
+++ Assets/CenterCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterCam : MonoBehaviour {
+
+	public float vitesse;
+	public GameObject  player;
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = player.transform.position + new Vector3(Input.GetAxis("Horizontal")*vitesse, 0, 0);
+
+	}
+}
