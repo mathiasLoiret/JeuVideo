894a926de9e827536a907ebbb21faeedbf2b9fb5
Matthieu Fournier <mat.fournier85@gmail.com>
2018-03-16 08:34:07 +0100

    opti script collectable

--- Assets/collectableScripte.cs
+++ Assets/collectableScripte.cs
@@ -24,12 +24,14 @@ public class collectableScripte : MonoBehaviour
     void Update()
     {
         if (i > 0)
-            i += Time.deltaTime*2;
+        {
+            i += Time.deltaTime * 2;
+            tr.position = initPosition + new Vector3(0, CustumEase(i) * 2, 0);
+        }
+            
 
         if (i > 1 )
             Destroy(this.gameObject);
-
-        tr.position = initPosition + new Vector3(0, CustumEase(i)*2, 0);
 
     }
 
