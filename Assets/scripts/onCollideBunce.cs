0b9690cec82325ed35619a38498db5f9d1488593
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-03-16 09:16:04 +0100

    add bounce

---
+++ Assets/scripts/onCollideBunce.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCollideBunce : MonoBehaviour {
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		ContactPoint2D contact = other.contacts[0];
+		Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+		rb.AddForce(contact.normal * contact.relativeVelocity.magnitude * 0.6f, ForceMode2D.Impulse);
+	}
+}
